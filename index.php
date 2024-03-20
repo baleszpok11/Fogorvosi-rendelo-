8c94e5e0b65a2045877801585dd8cf8aba57d11e
baleszpok11 <balogbalesz1234@gmail.com>
2024-03-20 19:15:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
-<div id="navbar-top">
-
+<?php
+session_start();
+?>
+<head>
+    <title>Fogorvosi rendelő</title>
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="icon" type="image/x-icon" href="style/favicon_io/favicon-16x16.png">
+</head>
+<div class="navbar-top">
+    <a href="index.php">Főoldal</a>
+    <?php
+    if (!isset($_SESSION['jmbg'])) {
+        echo '<div class="login-register">
+            <a href="register.php">Regisztráció</a>
+            <a href="login.php">Bejelentkezés</a>
+    </div>';
+    } else {
+        echo '<div class ="logged"> <p>' . $_SESSION["jmbg"] . '</p>';
+    }
+    ?>
 </div>
