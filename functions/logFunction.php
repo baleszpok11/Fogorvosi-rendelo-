063457f8a0e7803288f8d6d32a3e0834eb9877d5
baleszpok11 <balogbalesz1234@gmail.com>
2024-03-20 19:15:44 +0100

    Create logFunction.php

---
+++ functions/logFunction.php
@@ -0,0 +1,43 @@
+<?php
+
+
+// Start session
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    require 'connection.php';
+    global $conn;
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $remember_me = isset($_POST['remember_me']) ? true : false;
+
+    $sql = "SELECT * FROM Patient WHERE email = '$email'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['jmbg'] = $row['jmbg'];
+
+            if ($remember_me) {
+                $token = bin2hex(random_bytes(32));
+                setcookie('remember_me_cookie', $token, time() + (86400 * 30), "/"); // 30 napos cookie
+                $sql = "UPDATE Patient SET token = '$token' WHERE jmbg = " . $row['jmbg'];
+                $conn->query($sql);
+            }
+
+            header("Location: ../index.php");
+            exit();
+        } else {
+            // Authentication failed
+            $error_message = "Invalid email or password.";
+        }
+    } else {
+        // User not found
+        $error_message = "Invalid email or password.";
+    }
+}
+
+// Close database connection
+$conn->close();
+?>
