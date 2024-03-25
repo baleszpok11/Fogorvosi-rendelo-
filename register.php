17135efe37fd7212efae637d549d07101e5f9ec8
baleszpok11 <balogbalesz1234@gmail.com>
2024-03-25 01:38:48 +0100

    Update register.php

--- register.php
+++ register.php
@@ -4,62 +4,38 @@
     <link rel="stylesheet" href="style/style.css">
     <link rel="icon" type="image/x-icon" href="style/favicon_io/favicon-16x16.png">
 </head>
+<body>
+    <a href="index.php">Kezdőlap</a>
+    <a href="register.php">Regisztráció</a>
+    <a href="login.php">Bejelentkezés</a>
+<form action="functions/regFunction.php" method="POST" onsubmit="return validateForm()">
+    <h2>Regisztráció</h2>
+    <label for="jmbg"></label>
+    <input type="text" id="jmbg" name="jmbg" placeholder="JMBG" required><br><br>
+    <br>
+    <label for="firstName"></label>
+    <input type="text" id="firstName" name="firstName" placeholder="Keresztnév" required><br><br>
+    <br>
+    <label for="lastName"></label>
+    <input type="text" id="lastName" name="lastName" placeholder="Vezetéknév" required><br><br>
+    <br>
+    <label for="phoneNumber"></label>
+    <input type="text" id="phoneNumber" name="phoneNumber" placeholder="Telefonszám" required><br><br>
+    <br>
+    <label for="email"></label>
+    <input type="email" id="email" name="email" placeholder="Email cím" required><br><br>
+    <small id="emailHelpBlock" style="display: none; color: red;">Kérem, adjon meg egy érvényes e-mail címet.</small>
+    <br>
+    <label for="password"></label>
+    <input type="password" id="password" name="password" placeholder="Jelszó" required><br><br>
+    <small id="passwordHelpBlock" style="display: none; color: red;">A jelszónak legalább egy számot, egy nagybetűt, és egy speciális karaktert (pl. !) kell tartalmaznia.</small>
+    <br>
+    <label for="passwordConfirm"></label>
+    <input type="password" id="passwordConfirm" name="passwordConfirm" placeholder="Jelszó megerősítése" required>
+    <small id="passwordConfirmHelpBlock" style="display: none; color: red;">A jelszó és a megerősítés nem egyezik meg.</small><br><br>
+    <br>
+    <input type="submit" value="Regisztráció" name="register" id="registerBtn" disabled>
+</form>
 
-<div id="container">
-    <div class="header">
-
-        <div class="title">
-            <h1>Fogorvosi rendelő</h1>
-        </div>
-        <div class="info">
-
-            <div class="menu">
-                <ul>
-                    <li><a href="index.php">Kezdőlap</a></li>
-                    <li><a href="#">Áraink</a></li>
-                    <li><a href="#">Időpont foglalás</a></li>
-                    <li><a href="register.php">Regisztráció</a></li>
-                    <li><a href="login.php">Bejelentkezés</a></li>
-                </ul>
-            </div>
-
-        </div>
-
-    </div>
-    <hr>
-</div>
-
-<div class="register-login">
-    <form action="functions/regFunction.php" method="POST">
-        <h2>Regisztráció</h2>
-        <label for="jmbg"></label>
-        <input type="text" id="jmbg" name="jmbg" placeholder="JMBG" required><br><br>
-        <br>
-        <label for="firstName"></label>
-        <input type="text" id="firstName" name="firstName" placeholder="Keresztnév" required><br><br>
-        <br>
-        <label for="lastName"></label>
-        <input type="text" id="lastName" name="lastName" placeholder="Vezetéknév" required><br><br>
-        <br>
-        <label for="phoneNumber"></label>
-        <input type="text" id="phoneNumber" name="phoneNumber" placeholder="Telefonszám" required><br><br>
-        <br>
-        <label for="email"></label>
-        <input type="email" id="email" name="email" placeholder="Email cím" required><br><br>
-        <small id="emailHelpBlock" style="display: none; color: red;">Kérem, adjon meg egy érvényes e-mail
-            címet.</small>
-        <br>
-        <label for="password"></label>
-        <input type="password" id="password" name="password" placeholder="Jelszó" required><br><br>
-        <small id="passwordHelpBlock" style="display: none; color: red;">A jelszónak legalább egy számot, egy nagybetűt,
-            és egy speciális karaktert (pl. !) kell tartalmaznia.</small>
-        <br>
-        <label for="passwordConfirm"></label>
-        <input type="password" id="passwordConfirm" name="passwordConfirm" placeholder="Jelszó megerősítése" required>
-        <small id="passwordConfirmHelpBlock" style="display: none; color: red;">A jelszó és a megerősítés nem egyezik
-            meg.</small><br><br>
-        <br>
-        <script src="functions/register.js"></script>
-        <input type="submit" value="Regisztráció" name="register">
-    </form>
-</div>
+<script src="functions/register.js"></script>
+</body>
