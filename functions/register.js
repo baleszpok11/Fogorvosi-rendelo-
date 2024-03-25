443de5be44ccafb754e66ce47c60902a36944620
baleszpok11 <balogbalesz1234@gmail.com>
2024-03-25 01:39:03 +0100

    Update register.js

--- functions/register.js
+++ functions/register.js
@@ -1,79 +1,59 @@
-var emailInput = document.getElementById("email");
-var emailHelpBlock = document.getElementById("emailHelpBlock");
-var passwordInput = document.getElementById("password");
-var passwordConfirmInput = document.getElementById("passwordConfirm");
-var passwordHelpBlock = document.getElementById("passwordHelpBlock");
-var passwordConfirmHelpBlock = document.getElementById("passwordConfirmHelpBlock");
-
-// Email validation
-emailInput.addEventListener("input", function () {
-    var email = emailInput.value;
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (!emailRegex.test(email)) {
-        emailHelpBlock.style.display = "block";
-    } else {
-        emailHelpBlock.style.display = "none";
-    }
-});
-
-// Password validation
-passwordInput.addEventListener("input", function () {
-    var password = passwordInput.value;
-    var numberRegex = /\d/;
-    var uppercaseRegex = /[A-Z]/;
-    var specialCharRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
-
-    if (!numberRegex.test(password) || !uppercaseRegex.test(password) || !specialCharRegex.test(password)) {
-        passwordHelpBlock.style.display = "block";
-    } else {
-        passwordHelpBlock.style.display = "none";
-    }
-});
-
-// Password confirmation validation
-passwordConfirmInput.addEventListener("input", function () {
-    var password = passwordInput.value;
-    var passwordConfirm = passwordConfirmInput.value;
-
-    if (password !== passwordConfirm) {
-        passwordConfirmHelpBlock.style.display = "block";
-    } else {
-        passwordConfirmHelpBlock.style.display = "none";
-    }
-});
-
 function validateForm() {
+    var emailInput = document.getElementById("email");
+    var emailHelpBlock = document.getElementById("emailHelpBlock");
+    var passwordInput = document.getElementById("password");
+    var passwordConfirmInput = document.getElementById("passwordConfirm");
+    var passwordHelpBlock = document.getElementById("passwordHelpBlock");
+    var passwordConfirmHelpBlock = document.getElementById("passwordConfirmHelpBlock");
+    var registerBtn = document.getElementById("registerBtn");
+
     var email = emailInput.value;
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (!emailRegex.test(email)) {
-        emailHelpBlock.style.display = "block";
-        return false;
-    } else {
-        emailHelpBlock.style.display = "none";
-    }
-
     var password = passwordInput.value;
     var numberRegex = /\d/;
     var uppercaseRegex = /[A-Z]/;
     var specialCharRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+    var passwordConfirm = passwordConfirmInput.value;
 
-    if (!numberRegex.test(password) || !uppercaseRegex.test(password) || !specialCharRegex.test(password)) {
+    var validEmail = emailRegex.test(email);
+    var validPassword = numberRegex.test(password) && uppercaseRegex.test(password) && specialCharRegex.test(password);
+    var passwordMatch = password === passwordConfirm;
+
+    if (!validEmail) {
+        emailHelpBlock.style.display = "block";
+    } else {
+        emailHelpBlock.style.display = "none";
+    }
+
+    if (!validPassword) {
         passwordHelpBlock.style.display = "block";
-        return false;
     } else {
         passwordHelpBlock.style.display = "none";
     }
 
-    var passwordConfirm = passwordConfirmInput.value;
-
-    if (password !== passwordConfirm) {
+    if (!passwordMatch) {
         passwordConfirmHelpBlock.style.display = "block";
-        return false;
     } else {
         passwordConfirmHelpBlock.style.display = "none";
     }
 
-    return true;
+    if (validEmail && validPassword && passwordMatch) {
+        registerBtn.disabled = false;
+        return true;
+    } else {
+        registerBtn.disabled = true;
+        return false;
+    }
 }
+
+window.addEventListener("load", function () {
+    var emailInput = document.getElementById("email");
+    var passwordInput = document.getElementById("password");
+    var passwordConfirmInput = document.getElementById("passwordConfirm");
+
+    emailInput.addEventListener("input", validateForm);
+    passwordInput.addEventListener("input", validateForm);
+    passwordConfirmInput.addEventListener("input", validateForm);
+
+    validateForm();
+});
