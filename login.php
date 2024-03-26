d9b036645600f14ea86b3915156fd574c1fa0716
vikiboy212 <viktorsoregi@gmail.com>
2024-03-26 15:22:50 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <head>
     <title>Bejelentkezés</title>
     <link rel="icon" type="image/x-icon" href="style/favicon_io/favicon-16x16.png">
+    <link rel="stylesheet" href="style/style.css">
 </head>
 <div class="navbar">
     <ul>
@@ -9,6 +10,13 @@
         <li><a href="login.php">Bejelentkezés</a></li>
     </ul>
 </div>
+<div class="header">
+
+    <div class="title">
+        <h1>Bejelentkezés</h1>
+    </div>
+</div>
+<div class="container">
 <form action="functions/logFunction.php" method="post">
     <label for="email"></label>
     <input type="email" id="email" name="email" placeholder="Email" required><br><br>
@@ -16,8 +24,12 @@
     <label for="password"></label>
     <input type="password" id="password" name="password" placeholder="Jelszó" required><br><br>
 
-    <input type="checkbox" id="remember_me" name="remember_me">
-    <label for="remember_me">Remember Me</label><br><br>
+    <label for="remember_me">
+        Remember Me
+        <input type="checkbox" id="remember_me" name="remember_me">
+    </label><br><br>
+
 
     <input type="submit" value="Login">
 </form>
+</div>
