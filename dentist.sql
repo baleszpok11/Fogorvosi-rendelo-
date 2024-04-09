5bf2f32c1bcf436ec51c18671a344370cafb15f3
baleszpok11 <161521648+baleszpok11@users.noreply.github.com>
2024-04-09 16:30:41 +0200

    Update dentist.sql

--- dentist.sql
+++ dentist.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: localhost
--- Létrehozás ideje: 2024. Ápr 09. 16:18
+-- Létrehozás ideje: 2024. Ápr 09. 16:29
 -- Kiszolgáló verziója: 10.4.28-MariaDB
 -- PHP verzió: 8.2.4
 
@@ -24,36 +24,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `doctor`
+-- Tábla szerkezet ehhez a táblához `Doctor`
 --
 
-CREATE TABLE `doctor` (
-  `doctorID` int(11) NOT NULL,
-  `firstName` varchar(50) NOT NULL,
-  `lastName` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `phoneNumber` varchar(20) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `specialization` varchar(100) NOT NULL,
-  `forget` varchar(255) NOT NULL,
-  `remember` varchar(255) NOT NULL
+CREATE TABLE `Doctor` (
+                          `doctorID` int(11) NOT NULL,
+                          `firstName` varchar(50) NOT NULL,
+                          `lastName` varchar(50) NOT NULL,
+                          `password` varchar(255) NOT NULL,
+                          `phoneNumber` varchar(20) NOT NULL,
+                          `email` varchar(100) NOT NULL,
+                          `specialization` varchar(100) NOT NULL,
+                          `forget` varchar(255) NOT NULL,
+                          `remember` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `patient`
+-- Tábla szerkezet ehhez a táblához `Patient`
 --
 
-CREATE TABLE `patient` (
-  `patientID` int(11) NOT NULL,
-  `firstName` varchar(50) NOT NULL,
-  `lastName` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `phoneNumber` varchar(20) DEFAULT NULL,
-  `email` varchar(100) NOT NULL,
-  `forgot` varchar(255) DEFAULT NULL,
-  `remember` varchar(255) DEFAULT NULL
+CREATE TABLE `Patient` (
+                           `patientID` int(11) NOT NULL,
+                           `firstName` varchar(50) NOT NULL,
+                           `lastName` varchar(50) NOT NULL,
+                           `password` varchar(255) NOT NULL,
+                           `phoneNumber` varchar(20) DEFAULT NULL,
+                           `email` varchar(100) NOT NULL,
+                           `forgot` varchar(255) DEFAULT NULL,
+                           `remember` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -61,34 +61,34 @@ CREATE TABLE `patient` (
 --
 
 --
--- A tábla indexei `doctor`
+-- A tábla indexei `Doctor`
 --
-ALTER TABLE `doctor`
-  ADD PRIMARY KEY (`doctorID`),
-  ADD UNIQUE KEY `email` (`email`);
+ALTER TABLE `Doctor`
+    ADD PRIMARY KEY (`doctorID`),
+    ADD UNIQUE KEY `email` (`email`);
 
 --
--- A tábla indexei `patient`
+-- A tábla indexei `Patient`
 --
-ALTER TABLE `patient`
-  ADD PRIMARY KEY (`patientID`),
-  ADD UNIQUE KEY `email` (`email`);
+ALTER TABLE `Patient`
+    ADD PRIMARY KEY (`patientID`),
+    ADD UNIQUE KEY `email` (`email`);
 
 --
 -- A kiírt táblák AUTO_INCREMENT értéke
 --
 
 --
--- AUTO_INCREMENT a táblához `doctor`
+-- AUTO_INCREMENT a táblához `Doctor`
 --
-ALTER TABLE `doctor`
-  MODIFY `doctorID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `Doctor`
+    MODIFY `doctorID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT a táblához `patient`
+-- AUTO_INCREMENT a táblához `Patient`
 --
-ALTER TABLE `patient`
-  MODIFY `patientID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `Patient`
+    MODIFY `patientID` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
