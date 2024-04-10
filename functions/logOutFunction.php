51577d5879a6311e23f016a1dce0f234f4561f28
baleszpok11 <161521648+baleszpok11@users.noreply.github.com>
2024-04-10 13:46:35 +0200

    Update logOutFunction.php

--- functions/logOutFunction.php
+++ functions/logOutFunction.php
@@ -1,10 +1,13 @@
 <?php
 session_start();
-
+require 'connection.php';
+global $conn;
 // cookie törlése
 if (isset($_COOKIE['remember_me_cookie'])) {
     unset($_COOKIE['remember_me_cookie']);
     setcookie('remember_me_cookie', '', ['expires' => time() - 3600, 'path' => '/']);
+    $sql = "UPDATE Patient SET remember = NULL WHERE patientID = " . $_SESSION['patientID'];
+    $conn->query($sql);
 }
 
 // session törlése
